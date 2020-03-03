17402242c5453be3074f98c86cca6e2a66dfa0bc
Nguyễn Hải Quang <haiquang9994@gmail.com>
2020-03-03 12:20:02 +0700

    fix bug Undefined offset: 1 when create GraphRawResponse

--- src/Http/GraphRawResponse.php
+++ src/Http/GraphRawResponse.php
@@ -87,8 +87,9 @@ class GraphRawResponse
      */
     public function setHttpResponseCodeFromHeader($rawResponseHeader)
     {
-        preg_match('|HTTP/\d\.\d\s+(\d+)\s+.*|', $rawResponseHeader, $match);
-        $this->httpResponseCode = (int)$match[1];
+        if (preg_match("/^HTTP.*(\d+)$/", trim($rawResponseHeader), $match) && isset($match[1])) {
+            $this->httpResponseCode = intval($match[1]);
+        }
     }
 
     /**
